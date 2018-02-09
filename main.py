1044b204f0c69bb869802d95cb7467dde7e6151c
JelenaZoric <zoric.jelena95@gmail.com>
2018-02-09 14:13:25 +0100

    Dodavanje dela za prepoznavanje plamenjače

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import random
 import math
 import operator
 import cv2
+from sklearn.cluster import KMeans
 # from sklearn import neighbors
 file = open('podaci.csv', 'w')
 # 'hwratio,eccentricity,solidity,species'
@@ -23,13 +24,8 @@ for i in range(0, brojKr + brojVl):
     else:
         img = imread('slike/kr' + i.__str__() + '.jpg')
         vrsta = 1
-# imgResized = img.resize((width, height))
     hsl, wsl = img.shape[:2]
-#    plt.imshow(img)
-# kmeans = KMeans(n_clusters=2).fit(img)
-# kmeans.predict(img)
     imgGray = rgb2gray(img)
-#    plt.imshow(imgGray, 'gray')
     thresh = threshold_otsu(imgGray)
     imgTreshold = imgGray <= thresh
 # imgTreshold = 1 - threshold_adaptive(imgGray, block_size=75, offset=0.04)
@@ -37,7 +33,6 @@ for i in range(0, brojKr + brojVl):
     str_elem = disk(9)
     imgClosed = closing(imgTreshold, str_elem)
     imgClosed2 = closing(imgClosed, str_elem)
-#    plt.imshow(imgClosed2, 'gray')
     labeledImg = label(imgClosed2)
     regions = regionprops(labeledImg)
     print('Regioni:{}'.format(len(regions)))
@@ -77,9 +72,67 @@ for i in range(0, brojKr + brojVl):
                 print('biljka je krompir')
 #             plt.imshow(draw_regions(regions_psenica, imgClosed2.shape), 'gray')
 # print(txt_to_write)
+
+    # deo za prepoznavanje plamenjace
+    image = img.reshape((img.shape[0] * img.shape[1], 3))
+
+    clt = KMeans(n_clusters=3)
+    clt.fit(image)
+
+    # grab the number of different clusters and create a histogram
+    # based on the number of pixels assigned to each cluster
+    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+    (hist, _) = np.histogram(clt.labels_, bins=numLabels)
+
+    # normalize the histogram, such that it sums to one
+    hist = hist.astype("float")
+    hist /= hist.sum()
+
+    # initialize the bar chart representing the relative frequency
+    # of each of the colors
+    bar = np.zeros((50, 300, 3), dtype="uint8")
+    startX = 0
+
+    # loop over the percentage of each cluster and the color of
+    # each cluster
+    for (percent, color) in zip(hist, clt.cluster_centers_):
+        # plot the relative percentage of each cluster
+        endX = startX + (percent * 300)
+        cv2.rectangle(bar, (int(startX), 0), (int(endX), 50), color.astype("uint8").tolist(), -1)
+        startX = endX
+
+    # plt.figure()
+    # plt.axis("off")
+    # plt.imshow(bar)
+
+    # print(bar)
+    # l = chain.from_iterable(zip(*bar))
+    # list(l)
+
+    # print(list(l))
+    width, height = bar.shape[:2]
+    pixel_values = list(bar)
+    # print(width)
+    # print(height)
+    pixel_values = np.array(pixel_values).reshape((width, height, 3))  # w = 50, h = 300
+    # print(pixel_values[1][1][2])  #u poslednjoj zagradi se kaze da li je r g ili b
+    ##pix = bar.load()
+    # for x in range(width):
+    for y in range(height):
+        # for z in range(3):
+
+        if pixel_values[1][y][2] > 70 and pixel_values[1][y][2] < 200:
+            print(pixel_values[1][y][2])
+            print('Biljka ima plamenjacu.')
+            break
+        else:
+            print('Biljka nema plamenjacu.')
+            break
 file.writelines(txt_to_write)
 file.close()
 #    plt.show()
+
+
 def load_dataset(filename, split, training_set=[], test_set=[]):
     with open(filename, 'rb') as csvfile:
         lines = csv.reader(csvfile)
@@ -128,10 +181,10 @@ def get_response(neighbors):  # vraca labele klasa najblizih suseda
 def get_accuracy(testSet, predictions):
     correct = 0
     for x in range(len(testSet)):
-       # for y in range(len(predictions[x])):
+        # for y in range(len(predictions[x])):
             if testSet[x][-1] == predictions[x]:
                 correct += 1
-               # break
+                # break
     return (correct / float(len(testSet))) * 100.0
 
 
@@ -158,25 +211,25 @@ def main():
     accuracy = get_accuracy(testSet, predictionsForK)
     print('Accuracy for k nearest: ' + repr(accuracy) + '%')
 
-    img = cv2.imread('slike/kr20.jpg')
-    Z = img.reshape((-1, 3))
+    # img = cv2.imread('slike/kr20.jpg')
+    # Z = img.reshape((-1, 3))
 
     # convert to np.float32
-    Z = np.float32(Z)
+    # Z = np.float32(Z)
 
     # define criteria, number of clusters(K) and apply kmeans()
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    K = 3
-    ret, label, center = cv2.kmeans(Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    # criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    # K = 3
+    # ret, label, center = cv2.kmeans(Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
     # Now convert back into uint8, and make original image
-    center = np.uint8(center)
-    res = center[label.flatten()]
-    res2 = res.reshape((img.shape))
+    # center = np.uint8(center)
+    # res = center[label.flatten()]
+    # res2 = res.reshape((img.shape))
 
-    cv2.imshow('res2', res2)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.imshow('res2', res2)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
 
 main()
